1c6fcf286c854f0cfa23bbd7b4484304c174104f
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-04-09 09:20:10 +0530

    add board representation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
+typedef enum
+{
+    PIECE_NONE = 0,
+    PIECE_PLAYER1,
+    PIECE_PLAYER2,
+} Piece;
+
+typedef struct
+{
+    Piece board[6][7];
+    Piece curr_player;
+    int col_heights[7];
+} Board;
+
+int main(void)
+{
     printf("connect4-engine\n");
     return 0;
 }
